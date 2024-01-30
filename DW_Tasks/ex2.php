6809d1f2750bfceaee72f167abc3caa8166c5976
dulshan.welengoda <dulshan.welengoda@gmail.com>
2024-01-30 21:29:34 +0200

    Initial commit

---
+++ DW_Tasks/ex2.php
@@ -0,0 +1,28 @@
+<?php 
+     $title ="Exercise 2: Creating Universal Header and Footer";
+include 'header.php'; ?>
+
+<section id="plan">
+            <div class="tourPlan text-center">
+                <div class="row">
+                    <div class="col">
+                        <h1>EXERSICE 2</h1>
+                    </div>
+                </div>
+                <div class="container text-left">
+                    <div class="row tourPlanCards">
+                        <div class="col-md-12">
+                         <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, optio a reprehenderit perferendis quo rerum officiis ipsam laborum ad necessitatibus totam cum, nihil dignissimos? 
+                            Facere ipsam ipsa omnis sapiente nisi?</p>
+                            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, optio a reprehenderit perferendis quo rerum officiis ipsam laborum ad necessitatibus totam cum, nihil dignissimos? 
+                            Facere ipsam ipsa omnis sapiente nisi?</p>
+                            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, optio a reprehenderit perferendis quo rerum officiis ipsam laborum ad necessitatibus totam cum, nihil dignissimos? 
+                            Facere ipsam ipsa omnis sapiente nisi?</p>
+                        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, optio a reprehenderit perferendis quo rerum officiis ipsam laborum ad necessitatibus totam cum, nihil dignissimos? 
+                        Facere ipsam ipsa omnis sapiente nisi?</p>
+
+    </div>
+    </div>
+        
+<?php include 'footer.php'; ?>
+       
